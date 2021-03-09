1ceba3eda18c16423571602cdd6cf41f84919842
Number279 <sawyercarroll@gmail.com>
2021-03-09 08:56:18 -0600

    first commit

---
+++ src/main/java/com/tts/deplomentdemo/DeplomentdemoApplication.java
@@ -0,0 +1,13 @@
+package com.tts.deplomentdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeplomentdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeplomentdemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tts/deplomentdemo/DeplomentdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tts.deplomentdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeplomentdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
